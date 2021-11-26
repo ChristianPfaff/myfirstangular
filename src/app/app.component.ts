58704be7a62d927c841e1195346d520a0676b2f7
Christian Pfaff <madadaskar@googlemail.com>
2021-11-26 21:27:14 +0100

    reworked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'christianpfaff';
+export class AppComponent implements OnInit {
+  images = ['Berghaupten.jpg', 'Gengenbach.jpg', 'Spargel.jpg'];
+  currentImage = 0;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(()=>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+
+    },8000);
+  }
 }
+
