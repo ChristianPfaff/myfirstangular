4d9d1367fa74df84afd4faacc58f75ecce8c8e8a
Christian Pfaff <madadaskar@googlemail.com>
2021-11-27 21:18:18 +0100

    reworked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,25 +5,4 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  images = ['Berghaupten.jpg', 'Gengenbach.jpg', 'Spargel.jpg'];
-  headlines = ['Berghaupten', 'Gengenbach', 'Spargel'];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage = true;
-      }, 10);
-    }, 8000);
-  }
-}
+export class AppComponent {}
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+
+  images = ['Berghaupten.jpg', 'Gengenbach.jpg', 'Spargel.jpg'];
+  headlines = ['Berghaupten', 'Gengenbach', 'Spargel'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+
+}
